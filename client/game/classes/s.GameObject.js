41ef9aae8e1ca122096315d07cbb54c69644b92b
Larry Davis <lazdnet@gmail.com>
2014-06-10 21:08:21 -0700

    Only accept quaternions for rotation, set position, rotation, velocity, angularVelocity if passed, clean up physics objects

--- client/game/classes/s.GameObject.js
+++ client/game/classes/s.GameObject.js
@@ -3,32 +3,74 @@ s.GameObject = new Class({
   extend: s.EventEmitter,
 
   construct: function(options) {
+    if (options.rotation) {
+      if (!(options.rotation instanceof THREE.Quaternion)) {
+        throw new Error('s.GameObject: options.rotation should be a THREE.Quaternion');
+      }
+    }
+
     // Bind execution scope of update, if necessary
     if (this.update)
       this.update = this.update.bind(this);
 
     // Store scene for remove
     this.game = options.game;
+
+    // Store options
+    this.options = options;
   },
 
   init: function() {
-    // Store a reference to the instance on the object
-    // This is used after a collision is detected
-    // For instance, to remove HP from the item hit
-    if (this.root)
-      this.root.instance = this;
+    if (this.root) {
+      // Position mesh in scene
+      if (this.options.position) {
+        this.root.position.copy(this.options.position);
+      }
+      if (this.options.rotation) {
+        this.root.quaternion.copy(this.options.rotation);
+      }
+    }
+
+    if (this.body) {
+      // Store a reference to the instance on the object
+      // This is used after a collision is detected
+      // For instance, to remove HP from the item hit
+      // this.body.instance = this;
+
+      // Position body in physics simulation
+      // Manually assign values so both CANNON and THREE math types are supported
+      if (this.options.position) {
+        this.body.position.set(this.options.position.x, this.options.position.y, this.options.position.z);
+      }
+      if (this.options.rotation) {
+        this.body.quaternion.set(this.options.rotation.x, this.options.rotation.y, this.options.rotation.z, this.options.rotation.w);
+      }
+      if (this.options.velocity) {
+        this.body.velocity.set(this.options.velocity.x, this.options.velocity.y, this.options.velocity.z);
+      }
+      if (this.options.angularVelocity) {
+        this.body.angularVelocity.set(this.options.angularVelocity.x, this.options.angularVelocity.y, this.options.angularVelocity.z);
+      }
+    }
 
     this.add();
   },
 
   destruct: function() {
     // Unhook from the rendering loop
-    if (this.update)
+    if (this.update) {
       this.game.unhook(this.update);
+    }
+
+    // Remove from physics simulation
+    if (this.body) {
+      this.game.world.remove(this.body);
+    }
 
     // Remove from the scene
-    if (this.root)
+    if (this.root) {
       this.game.scene.remove(this.root);
+    }
   },
 
   add: function() {
