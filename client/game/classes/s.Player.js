f35c3059c177bb4ce4bef940312890bd0758b68b
Larry Davis <lazdnet@gmail.com>
2014-06-10 21:09:24 -0700

    Selectable camera mode, delegate to s.GameObject for initial state setting, remove unused variables

--- client/game/classes/s.Player.js
+++ client/game/classes/s.Player.js
@@ -1,23 +1,26 @@
 s.Player = new Class({
   extend: s.Ship,
   construct: function(options) {
-    this.game = options.game;
     this.camera = options.camera;
-    this.HUD = options.HUD;
-    this.firstPerson = false;
     this.name = options.name || '';
-    this.initialize(options);
-
-    this.root.castShadow = true;
 
     // Root camera to the player's position
     this.root.add(this.camera);
 
-    // Setup camera: Cockpit view
-    // this.camera.position.set(0, 0, 0);
-
-    // Setup camera: Chase view
-    this.game.camera.position.set(0, 35, -250);
-    this.game.camera.lookAt(new THREE.Vector3(0,0,0));
+    // Set default view mode
+    this.setCameraViewMode();
+  },
+  
+  setCameraViewMode: function(mode) {
+    if (mode === 'firstPerson') {
+        // Setup camera: Cockpit view
+        // this.camera.position.set(0, 0, 0);
+        this.firstPerson = true;
+    }
+    else {
+        // Setup camera: Chase view
+        this.game.camera.position.set(0, 25, -250);
+        this.game.camera.lookAt(new THREE.Vector3(0,0,0));
+    }
   }
 });
--- client/game/classes/s.Ship.js
+++ client/game/classes/s.Ship.js
@@ -1,15 +1,13 @@
 s.Ship = new Class({
   extend: s.GameObject,
 
-  options: {  
-  },
-
-  initialize: function(options) {
+  construct: function(options) {
     var geometry = s.models[options.shipClass].geometry;
     this.materials = s.models[options.shipClass].materials[0];
     this.materials.emissive = new THREE.Color('rgb(255,255,255)');
 
     this.root = new THREE.Mesh(geometry, this.materials);
+    this.root.castShadow = true;
 
     // Cannon.js
     var shape = new CANNON.Sphere(100);
@@ -20,19 +18,10 @@ s.Ship = new Class({
     body.angularDamping = 0.99;
     body.linearDamping = 0.5;
 
-    // Position body in physics simulation
-    this.body.position.set(options.position.x, options.position.y, options.position.z);
-    this.body.quaternion.set(options.rotation.x, options.rotation.y, options.rotation.z, options.rotation.w);
-
-    // Position mesh in scene
-    this.root.position.copy(options.position);
-    this.root.quaternion.copy(options.rotation);
-
     this.lastTurretFire = 0;
     this.lastMissileFire = 0;
-    this.alliance = options.alliance;
+    this.team = options.team;
 
-    this.root.name = this.name;
     this.hull = s.config.ship.hull;
     this.shields = s.config.ship.shields;
 
