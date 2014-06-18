feaa4fa2acc3a695af30960814bee50df49d7ac2
Larry Davis <lazdnet@gmail.com>
2014-06-18 13:41:48 -0700

    Indentation

--- client/game/s.js
+++ client/game/s.js
@@ -4,39 +4,39 @@ Copyright (C) 2014 Contributors
 */
 
 window.s = {
-    config: {
-        sound: {
-            enabled: false, // @perf: Extremely slow on iOS
-            silentDistance: 10000,
-            sounds: {
-                laser: 'game/sounds/laser.mp3'
-            }
-        }
-    },
-
-    constants: {
-        ship: {
-            rollSpeed: Math.PI/8,
-            pitchSpeed: Math.PI/32,
-            yawSpeed: Math.PI/32,
-            forwardThrust: 25,
-            backwardThrust: 15,
-
-            linearDamping: 0.5,
-            angularDamping: 0.99
-        }
-    },
-
-    init: function() {
-        console.log('Satellite starting...');
-
-        // Create a projector for 2D <-> 3D calculations
-        s.projector = new THREE.Projector();
-
-        // Create a model loader
-        s.loader = new THREE.JSONLoader();
-
-        // Start the game
-        s.game = new s.SatelliteGame();
+  config: {
+    sound: {
+      enabled: false, // @perf: Extremely slow on iOS
+      silentDistance: 10000,
+      sounds: {
+        laser: 'game/sounds/laser.mp3'
+      }
     }
+  },
+
+  constants: {
+    ship: {
+      rollSpeed: Math.PI/8,
+      pitchSpeed: Math.PI/32,
+      yawSpeed: Math.PI/32,
+      forwardThrust: 25,
+      backwardThrust: 15,
+
+      linearDamping: 0.5,
+      angularDamping: 0.99
+    }
+  },
+
+  init: function() {
+    console.log('Satellite starting...');
+
+    // Create a projector for 2D <-> 3D calculations
+    s.projector = new THREE.Projector();
+
+    // Create a model loader
+    s.loader = new THREE.JSONLoader();
+
+    // Start the game
+    s.game = new s.SatelliteGame();
+  }
 };
